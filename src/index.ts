8ca21fa3459cee2a3d85228da076e949c8f4b08a
annaaz <annaazannaavi11@gmail.com>
2023-01-17 17:32:46 +0700

    Add init for Husty setup

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ app.use('/api/v1/main', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: 'REST is running' })
 })
 app.listen(port, () => {
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   console.log(`Server listening on port ${port}`)
 })
