2f7f4c3fd0e665a6c1a8ac5eb60e1a2b24d61e1d
annaaz <annaazannaavi11@gmail.com>
2023-01-17 17:28:33 +0700

    Add init for Husty setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, {Application,Request,Response,NextFunction} from "express";
+import express, { Application, NextFunction, Request, Response } from 'express'
 
-const app:Application = express()
-const port:Number = 4000
+const app: Application = express()
+const port: Number = 4000
 
-app.use("/api/v1/main", (req: Request, res: Response, next: NextFunction) =>{
-    res.status(200).send({data:'REST is running'})
-
-
+app.use('/api/v1/main', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: 'REST is running' })
 })
-app.listen(port,()=>console.log(`Server listening on port ${port}`))
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
