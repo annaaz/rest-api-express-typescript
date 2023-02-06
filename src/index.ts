2c1e179fcd4d8eb5a21f3008cc2cca151ad29b86
annaaz <annaazannaavi11@gmail.com>
2023-02-06 15:35:06 +0700

    REST API | Init REST API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
+import express, { Application } from 'express'
+import { routes } from './routes'
+import { logger } from './utils/logger'
 
 const app: Application = express()
 const port: Number = 4000
 
-app.use('/api/v1/main', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: 'REST is running' })
-})
+routes(app)
 app.listen(port, () => {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  console.log(`Server listening on port ${port}`)
+  logger.info(`Server listening on port ${port}`)
 })
