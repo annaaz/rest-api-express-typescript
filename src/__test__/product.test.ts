75145410e50fbac38d4c412999df3713fe1b5a60
annaaz <annaazannaavi11@gmail.com>
2023-11-07 17:38:53 +0700

    feat(restapi-express-typescript): add readme and latest changes

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -1,21 +1,33 @@
+import express from 'express'
+import { Express } from 'express-serve-static-core'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import * as mongoose from 'mongoose'
 import supertest from 'supertest'
 
 describe('product', () => {
+  let app: Express
+
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create()
     await mongoose.connect(mongoServer.getUri())
+
+    // Create an Express app for testing
+    app = express()
+    // Configure your routes and middleware here
   })
+
   afterAll(async () => {
     await mongoose.disconnect()
     await mongoose.connection.close()
   })
+
   describe('get detail product', () => {
     describe('given the product doesnt exist', () => {
       it('should return 404 and empty data', async () => {
-        const productID = 'proudct123'
-        await supertest().get(`/products/${productID}`).expect(404)
+        const productID = 'product123'
+        await supertest(app) // Use the Express app as an argument
+          .get(`/products/${productID}`)
+          .expect(404)
       })
     })
   })
